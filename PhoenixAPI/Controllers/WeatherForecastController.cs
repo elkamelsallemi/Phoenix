11d7090f3be72bb5c2377569b9151490fffc1cba
elkamelsallemi <elkamel.sallemi@gmail.com>
2023-12-12 01:23:08 +0100

    Update the project with models package

--- PhoenixAPI/Controllers/WeatherForecastController.cs
+++ PhoenixAPI/Controllers/WeatherForecastController.cs
@@ -20,7 +20,7 @@ namespace PhoenixAPI.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet("GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
