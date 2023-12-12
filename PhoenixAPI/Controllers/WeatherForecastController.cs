889b4093cdd565b6536e297c14aafd867dfb4ae2
elkamelsallemi <elkamel.sallemi@gmail.com>
2023-12-12 19:11:59 +0100

    Update controller API

--- PhoenixAPI/Controllers/WeatherForecastController.cs
+++ PhoenixAPI/Controllers/WeatherForecastController.cs
@@ -8,32 +8,8 @@ namespace PhoenixAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-/*        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet("GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }*/
-
         [HttpGet("GetTemperature")]
-        public string GetTemperature(int id)
+        public IActionResult GetTemperature(int id)
         {
             Temperature temp = new Temperature()
             {
@@ -41,7 +17,7 @@ namespace PhoenixAPI.Controllers
                 Temp = 25
             };
 
-            return temp.ToString();
+            return Ok(temp);
         }
     }
 }
